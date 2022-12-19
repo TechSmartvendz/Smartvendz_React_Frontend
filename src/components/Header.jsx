52122eb7200bdd2addc94ccc93fbe61b68183151
Kuldeep Malviya <kuldeep@smartvendz.com>
2022-12-19 09:37:53 +0530

    again test1 master

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
     <div className="header">
       
       
-      {/* <div className="headercontent" id="time">Fri Dec 09 2022 09:35:30 GMT +0530(India Standard Time)</div>   */}
+      <div className="headercontent" id="time">Fri Dec 09 2022 09:35:30 GMT +0530(India Standard Time)</div>   
       <div className="headercontent">
         {/* <img class="logo-main scale-with-grid" src="http://snaxsmart.com/wp-content/uploads/2018/05/logo-1.png" data-retina="http://snaxsmart.com/wp-content/uploads/2018/05/logo-1.png" data-height="88" alt="snacks vending machine" /> */}
          {/* <img src="/snaxsmart.png"/>  */}
