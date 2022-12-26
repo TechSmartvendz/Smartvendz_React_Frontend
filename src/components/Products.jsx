3355b7d5880d7776497b9fa543db27bd672cc371
Shubham Gupta <shubham.sg5000@gmail.com>
2022-12-26 15:16:25 +0530

    Added csv upload feature 26/12/2022

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,21 +1,17 @@
 import React,{useState} from 'react'
 import axios from 'axios'
-import { useCookies } from 'react-cookie';
 import fileDownload from 'js-file-download';
+import FileUpload from './FileUpload';
 
 
-// axios.defaults.headers.common['X-Auth-Token'] =
-//   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzgwNTBjYzg3ZDFiZjQwMmU3N2M1YzAiLCJpYXQiOjE2NjkzNTM2NzZ9.VNIOp891lpJXIzp0SZm8U503yS4W5VQmtt1fn1xrQ8g';
 
-// var cookieValue = document.cookie.replace(/(?:(?:^|.*;\s*)username\s*\=\s*([^;]*).*$)|^.*$/, "$1");
-// console.log(cookieValue)
 
 var urlz = "http://localhost:3000/productcsv/SVZBLR0099";
 var token ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzgwNTBjYzg3ZDFiZjQwMmU3N2M1YzAiLCJpYXQiOjE2NjkzNTM2NzZ9.VNIOp891lpJXIzp0SZm8U503yS4W5VQmtt1fn1xrQ8g";                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 // function download(response) {
                 
 
 function Products() {
-  
+
 function Download() {
   
   axios({
@@ -35,46 +31,11 @@ function Download() {
 
 }
 
-  //const[data, setData] = useState()
-  // const [message, setMessage] = useState("");
-  // const [cookies] = useCookies(['user']);
-  // const token = cookies.get("user");
-  // console.log(token);
-
-
-
-  // useEffect automatically executes once the page is fully loaded
-  // useEffect(() => {
-  //   // set configurations for the API call here
-  //   const configuration = {
-  //     method: "get",
-  //     url: "localhost:3000/productcsv/SVZBLR0099",
-  //     headers: {
-  //       Authorization: `Bearer ${cookievalue}`,
-  //     },
-  //   };
-
-  //   // make the API call
-  //   axios(configuration)
-  //     .then((result) => {
-  //       // assign the message in our result to the message we initialized above
-  //       setMessage(result.data.message);
-  //     })
-  //     .catch((error) => {
-  //       error = new Error();
-  //     });
-  // }, []);
-  // const products = async () => {
-  //  await axios.get("localhost:3000/productcsv/SVZBLR0099")
-  //   .then( res => res.json()  )
-  //   .catch( err => console.log(err))
-
-  // }
-
   return (
     <div>
       <p>Products</p>
       <button onClick={Download} style={{cursor: 'pointer'}}>Download as CSV</button>
+      <FileUpload />
 
     </div>
   )
