3f5f78c9dc5dc22d30b90b8a4265e53cb3e56f09
Sharanspujari <sharanspujari142@gmail.com>
2023-01-09 16:15:26 +0530

    added stype to usemanage buttons

--- src/Route/SubLink.jsx
+++ src/Route/SubLink.jsx
@@ -1,23 +1,26 @@
 import React, { useState } from "react";
 import {Link, Outlet } from "react-router-dom";
-
+import '../components/Usermanage/UserManage.css'
 function SubLink(props) {
   const [Sublinks,setSubLinks]=useState(props.sublinks)
   return (
     <div>
-    <div>
-    <ul>
+   
+ 
       {Sublinks.map((item, index) => {
         return (
+        
           <Link key={index} to={item.path}>
-          <button key={index}>
+          <button className="user_btn" key={index}>
            {item.title}
           </button>
         </Link> 
+      
+      
         );
       })}
-    </ul>
-    </div>
+    
+    
       <Outlet/>
     </div>
   );
--- src/components/UserManage.jsx
+++ src/components/UserManage.jsx
@@ -1,7 +1,7 @@
 import React,{useState} from 'react'
 import SubLink from '../Route/SubLink';
 import {UserManagerData} from '../Route/SubLinkJSON';
-
+import './Usermanage/UserManage.css'
 
 
 function UserManage() {
@@ -9,7 +9,9 @@ const [subLinkJSON,setSubLinkJSON]=useState(UserManagerData);
   return (
    <React.Fragment>
      <div className='mesgarea'></div>
+     <div className='btn_container'>
       <SubLink sublinks={subLinkJSON} />
+      </div>
       </React.Fragment>
   )
 }
