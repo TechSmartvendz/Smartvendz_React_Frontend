c61f0193ab5c9c076ebe8097d31883b4a654813a
RakeshAhire <rocky5509ahire@gmail.com>
2023-08-04 10:38:30 +0530

    admin approve request implemented

--- src/components/RefillerManage/RefillerManager.jsx
+++ src/components/RefillerManage/RefillerManager.jsx
@@ -2,12 +2,11 @@ import React, { useState } from "react";
 import SubLink from '../../Route/SubLink';
 import { RefillersManager } from "../../Route/SubLinkJSON";
 
-function RefillerManager() {
+const RefillerManager=()=> {
 
 const [subLinkJSON,setSubLinkJSON]=useState(RefillersManager);
   return (
     <React.Fragment>
-    
       <SubLink sublinks={subLinkJSON} />
     </React.Fragment>
   );
